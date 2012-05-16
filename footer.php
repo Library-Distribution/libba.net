83ada9e88039ff9fdc960c19b548b71ccf68ed95
maul.esel <maul.esel@go4more.de>
2012-05-16 18:16:15 +0200

    rework footer

--- footer.php
+++ footer.php
@@ -5,8 +5,15 @@
 			<td>
 				<ul>
 					<li><a href="index.php">Start page</a></li>
-					<li><a href="manual.php">Help</a></li>
-					<li><a href="faq.php">Frequently Asked Questions</a></li>
+					<li><a href="help.php">Help</a></li>
+					<li><a href="about.php">About</a></li>
+				</ul>
+			</td>
+			<td>
+				<ul>
+					<li><a href="viewuser.php">Users</a></li>
+					<li><a href="login.php?mode=login">Login</a> or <a href="login.php?mode=register">Register</a></li>
+					<li><a href="upload.php">Upload a new app or lib</a></li>
 				</ul>
 			</td>
 			<td>
@@ -14,11 +21,6 @@
 					<li><a href="http://www.autohotkey.proboards.com">Discussion forum</a></li>
 					<li><a href="http://www.autohotkey.com/comunity">AutoHotkey forum</a></li>
 					<li><a href="http://de.autohotkey.com/forum">AutoHotkey forum (DE)</a></li>
-				</ul>
-			</td>
-			<td>
-				<ul>
-					<li><a href="about.php">About</a></li>
 				</ul>
 			</td>
 		</tr>
