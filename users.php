a2134fbdfb1622e6be798ae037c814066fc4193f
maul.esel <maul.esel@go4more.de>
2012-05-16 18:51:45 +0200

    users.php: do not declare account as valid if activationToken is set

--- users.php
+++ users.php
@@ -1,18 +1,22 @@
 <?php
-	function validateLogin($user_name, $user_pw)
+	function validateLogin($user, $pw)
 	{
 		global $db_table_users;
 		$db_connection = db_ensureConnection();
 
-		$user_pw = hash("sha256", $user_pw);
-		$escaped_user = mysql_real_escape_string($user_name, $db_connection);
+		$pw = hash("sha256", $pw);
+		$escaped_user = mysql_real_escape_string($user, $db_connection);
 
-		$db_query = "SELECT pw FROM $db_table_users WHERE name = '$escaped_user'";
+		$db_query = "SELECT pw, activationToken FROM $db_table_users WHERE name = '$escaped_user'";
 		$db_result = mysql_query($db_query, $db_connection)
 		or die ("Could not find the specified user name.");
 
-		$pw = mysql_fetch_object($db_result)->pw;
-		if ($pw != $user_pw)
+		$data = mysql_fetch_object($db_result);
+		if ($data->activationToken != "")
+		{
+			die ("Account is currently deactivated.");
+		}
+		if ($data->pw != $pw)
 		{
 			die ("Invalid password was specified.");
 		}
