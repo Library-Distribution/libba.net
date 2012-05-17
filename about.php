fc272aecc683207c371eef507ade28aadef309b6
maul.esel <maul.esel@go4more.de>
2012-05-17 13:46:06 +0200

    corrected about link to Quicksand

--- about.php
+++ about.php
@@ -13,7 +13,7 @@
 				It's entirely written by me, maul.esel, and has been tested and developed on <a href="http://www.apachefriends.org/de/xampp.html">XAMPP</a> on Windows.
 			</p>
 			<p>
-				For better visual presentation, Andrew Paglinawan's <a href="www.fontsquirrel.com/fonts/Quicksand">&quot;Quicksand&quot;</a> font is used. It is licensed under the <a href="font/license.txt">SIL Open Font License, Version 1.1</a>.
+				For better visual presentation, <a href="http://andrewpaglinawan.com/#quicksand">Andrew Paglinawan's &quot;Quicksand&quot; font</a> is used. It is licensed under the <a href="font/license.txt">SIL Open Font License, Version 1.1</a>.
 				Many thanks for this wonderful work!
 			</p>
 		</div>
