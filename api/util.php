507fbbaa27c042719cf25748754180ca94da3df7
maul.esel <maul.esel@go4more.de>
2012-06-22 19:51:16 +0200

    add a function for searching a mutlidimensional array

--- api/util.php
+++ api/util.php
@@ -333,4 +333,20 @@
 		}
 		rmdir($dir);
 	}
+
+	# SOURCE: http://stackoverflow.com/a/6303043
+	# modified to allow an array of key-value pairs instead of just one
+	function searchSubArray(Array $array, $key_values)
+	{
+		foreach ($array as $index => $subarray)
+		{
+			$match = true;
+			foreach ($key_values AS $key => $value)
+			{
+				$match = $match && (isset($subarray[$key]) && $subarray[$key] == $value);
+			}
+			if ($match)
+				return $index;
+		}
+	}
 ?>
