5d1dcd8500aedd2f84878e2617c6a7ae4d3f3b2b
maul.esel <maul.esel@go4more.de>
2012-06-28 21:36:39 +0200

    implement special version numbers for item description

--- api/items/describe.php
+++ api/items/describe.php
@@ -20,19 +20,43 @@
 
 				if (!isset($_GET["id"]))
 				{
-					$db_query = "SELECT HEX(id) FROM $db_table_main WHERE name = '" . mysql_real_escape_string($_GET["name"], $db_connection) . "' AND version = '" . mysql_real_escape_string($_GET["version"], $db_connection) . "'";
+					$version = mysql_real_escape_string($_GET["version"], $db_connection);
+
+					$db_version_cond = " AND version = '$version'";
+					if (in_array($version, array("latest", "first")))
+					{
+						$special_version = $version;
+						$db_version_cond = ""; # read all and filter below
+					}
+
+					$db_query = "SELECT HEX(id) FROM $db_table_main WHERE name = '" . mysql_real_escape_string($_GET["name"], $db_connection) . "' $db_version_cond";
 					$db_result = mysql_query($db_query, $db_connection);
 
 					if (!$db_result)
 					{
 						throw new HttpException(500);
 					}
-					if (mysql_num_rows($db_result) != 1)
+					if (mysql_num_rows($db_result) < 1)
 					{
 						throw new HttpException(404);
 					}
 
-					$db_entry = mysql_fetch_assoc($db_result);
+					if (isset($special_version)) # filter now if special version was used
+					{
+						$items = array(); # fetch all items in an array
+						while ($row = mysql_fetch_assoc($db_result))
+						{
+							$items[] = $row;
+						}
+
+						usort($items, "semver_sort"); # sort by "version" field, following semver rules
+						$db_entry = $items[$special_version == "latest" ? count($items) - 1 : 0];
+					}
+					else
+					{
+						$db_entry = mysql_fetch_assoc($db_result);
+					}
+
 					$id = $db_entry["HEX(id)"];
 				}
 				else
@@ -195,5 +219,10 @@
 			$content .= "</ald:version-list>";
 		}
 		return $content;
+	}
+
+	function semver_sort($a, $b)
+	{
+		return semver_compare($a["version"], $b["version"]);
 	}
 ?>
