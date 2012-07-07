d4a4fd57e62eb9715e2d5f87ad9bad71eb6fdab2
maul.esel <maul.esel@go4more.de>
2012-07-07 12:17:40 +0200

    [API] item listing: include unreviewed items only if explicitly specified (#10)

--- api/items/list.php
+++ api/items/list.php
@@ -43,6 +43,11 @@
 				$db_cond .= ($db_cond) ? " AND" : " WHERE";
 				$db_cond .= " default_include = '1'";
 			}
+			if (!isset($_GET["unreviewed"]) || !$_GET["unreviewed"] && strtolower($_GET["unreviewed"]) == "false")
+			{
+				$db_cond .= ($db_cond) ? " AND" : " WHERE";
+				$db_cond .= " reviewed = '1'";
+			}
 			if (isset($_GET["version"]))
 			{
 				$version = strtolower($_GET["version"]);
