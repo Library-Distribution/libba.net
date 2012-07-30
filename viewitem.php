e37179e4a5e9939583201d410ce8364295d2f337
maul.esel <maul.esel@go4more.de>
2012-07-30 16:44:14 +0200

    include index.php functionality in viewitem.php

--- viewitem.php
+++ viewitem.php
@@ -1,38 +1,61 @@
 <?php
 	session_start();
-	if (!isset($_GET["id"]))
-	{
-		header("Location: index");
-		exit;
-	}
-	$id = $_GET["id"];
 
-	require_once("sortArray.php");
 	require_once("ALD.php");
-	require_once("api/semver.php");
+	require_once("sortArray.php");
 
-	$api = new ALD(!empty($_SERVER["HTTPS"]) ? "https://{$_SERVER["SERVER_NAME"]}/user/maulesel/api" : "http://{$_SERVER["SERVER_NAME"]}/api");
-	try
-	{
-		$item = $api->getItemById($id);
-	}
-	catch (HttpException $e)
-	{
-		die ("Failed to retrieve information about this item.<p>{$e->getMessage()}</p>");
-	}
-
-	$page_title = "\"{$item['name']}\" (v{$item['version']})";
 	$logged_in = isset($_SESSION["user"]);
+	$api = new ALD(!empty($_SERVER["HTTPS"]) ? "https://{$_SERVER["SERVER_NAME"]}/user/maulesel/api" : "http://{$_SERVER["SERVER_NAME"]}/api");
 
-	function semver_sort($a, $b)
+	if (isset($_GET["id"])) # output an item
 	{
-		return semver_compare($b['version'], $a['version']);
+		require_once("api/semver.php");
+
+		$id = $_GET["id"];
+		try
+		{
+			$item = $api->getItemById($id);
+		}
+		catch (HttpException $e)
+		{
+			die ("Failed to retrieve information about this item.<p>{$e->getMessage()}</p>");
+		}
+
+		$page_title = "\"{$item['name']}\" (v{$item['version']})";
+	}
+	else # output a list of items
+	{
+		$page_title = "Browse ";
+
+		if ($type = (!empty($_GET["type"]) && in_array(strtolower($_GET["type"]), array("app", "lib"))) ? strtolower($_GET["type"]) : NULL)
+		{
+			$page_title .= ($type == "app") ? "applications" : ($type == "lib" ? "libraries" : "libraries and applications");
+		}
+		else # probably remove unknown type and reload?
+		{
+			$page_title .= "libraries and applications";
+		}
+
+		$user = !empty($_GET["user"]) ? $_GET["user"] : NULL
+			AND $page_title .= " by $user";
+		$stdlib = !empty($_GET["stdlib"]) ? $_GET["stdlib"] : "both"
+			AND $page_title .= !empty($_GET["stdlib"]) ? " (lib standard)" : "";
+		$tags = isset($_GET["tags"]) ? explode("|", $_GET["tags"]) : NULL
+			AND $page_title .= " (tags: " . implode($tags, ", ") . ")";
+
+		$page_index = !empty($_GET["page"]) ? (int)$_GET["page"] : 0;
+		$page_itemcount = !empty($_GET["items"]) ? (int)$_GET["items"] : 20;
+		$start_index = $page_index * $page_itemcount;
+
+		$items = $api->getItemList($start_index, $page_itemcount + 1, $type, $user, NULL, $tags, "latest", $stdlib);
+		$items = sortArray($items, "name");
 	}
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" href="default.css"/>
+		<link rel="stylesheet" type="text/css" href="default.css"/>
+		<?php if (!isset($id)) { ?><link rel="stylesheet" type="text/css" href="index.css"/><?php } ?>
 		<title><?php echo $page_title; ?></title>
 	</head>
 	<body>
@@ -40,45 +63,94 @@
 		<h1 id="page-title"><?php echo $page_title; ?></h1>
 		<div id="page-content">
 			<?php
-				if ($logged_in)
+				if (!isset($id))
 				{
-					require_once("privilege.php");
+					$last_letter = "";
+					$i = 0;
+					foreach ($items as $item)
+					{
+						$i++;
+						if ($i > $page_itemcount)
+						{
+							break;
+						}
 
-					$redirect_url = urlencode($_SERVER["REQUEST_URI"]);
-					if (hasPrivilege($_SESSION["privileges"], PRIVILEGE_REVIEW))
-					{
-						# insert review items
-						/*
-						echo "<div class=\"menu\">Code Review<ul class=\"admin-menu\">";
-						if ($item['reviewed'])
+						$current_letter = strtoupper(substr($item['name'], 0, 1));
+						if (!ctype_alpha($current_letter))
 						{
-							echo "<a href='moderator-action.php?id=$id&amp;action=review&amp;value=0&amp;return_error=true&amp;redirect=$redirect_url'><li>Mark as <span style=\"font-weight: bold;\">unreviewed</span></li></a>";
+							$current_letter = ".#?1";
 						}
-						else
+						if ($current_letter != $last_letter)
 						{
-							echo "<a href='moderator-action.php?id=$id&amp;action=review&amp;value=1&amp;return_error=true&amp;redirect=$redirect_url'><li>Mark as <span style=\"font-weight: bold; color: green\">secure and stable</span></li></a>";
+							if ($last_letter != "")
+							{
+								echo "</ul></div>";
+							}
+							echo "<div class='letter-container' id='items$current_letter'><span class='letter-item'>$current_letter</span><ul>";
 						}
-						echo "<a href='moderator-action.php?id=$id&amp;action=review&amp;value=-1&amp;return_error=true&amp;redirect=$redirect_url'><li>Mark as <span style=\"font-weight: bold; color: red\">unsecure or unstable</span></li></a>";
-						echo "</ul></div>";
-						*/
+						echo "<li id='item{$item['id']}'><a class='item' href='?id={$item['id']}'>{$item['name']}</a> (v{$item['version']}) by <a class='userlink' href='viewuser?user={$item['user']['name']}'>{$item['user']['name']}</a></li>";
+						$last_letter = $current_letter;
 					}
-					if (hasPrivilege($_SESSION["privileges"], PRIVILEGE_STDLIB) && $item['type'] == "lib" && $item['reviewed'])
+					if (count($items) > 0)
 					{
-						# insert default_include items
-						/*
-						echo "<div class=\"menu\">Library standard<ul class=\"admin-menu\">";
-						if ($item['default'])
-						{
-							echo "<a href='moderator-action.php?id=$id&amp;action=default&amp;value=0&amp;return_error=true&amp;redirect=$redirect_url'><li><span style=\"font-weight: bold; color: red\">Remove</span></li></a>";
-						}
-						else
-						{
-							echo "<a href='moderator-action.php?id=$id&amp;action=default&amp;value=1&amp;return_error=true&amp;redirect=$redirect_url'><li><span style=\"font-weight: bold; color: green\">Add</span></li></a>";
-						}
 						echo "</ul></div>";
-						*/
+					}
+					else
+					{
+						echo "<b>No items found that match your query.</b>";
+					}
+
+					if ($page_index > 0)
+					{
+						echo "<a class='next-previous' id='prev' href='?items=$page_itemcount&amp;page=".($page_index - 1)."'>Previous page</a>";
+					}
+
+					if (count($items) > $page_itemcount)
+					{
+						echo "<a class='next-previous' id='next' href='?items=$page_itemcount&amp;page=".($page_index + 1)."'>Next page</a>";
 					}
 				}
+				else
+				{
+					if ($logged_in)
+					{
+						require_once("privilege.php");
+
+						$redirect_url = urlencode($_SERVER["REQUEST_URI"]);
+						if (hasPrivilege($_SESSION["privileges"], PRIVILEGE_REVIEW))
+						{
+							# insert review items
+							/*
+							echo "<div class=\"menu\">Code Review<ul class=\"admin-menu\">";
+							if ($item['reviewed'])
+							{
+								echo "<a href='moderator-action.php?id=$id&amp;action=review&amp;value=0&amp;return_error=true&amp;redirect=$redirect_url'><li>Mark as <span style=\"font-weight: bold;\">unreviewed</span></li></a>";
+							}
+							else
+							{
+								echo "<a href='moderator-action.php?id=$id&amp;action=review&amp;value=1&amp;return_error=true&amp;redirect=$redirect_url'><li>Mark as <span style=\"font-weight: bold; color: green\">secure and stable</span></li></a>";
+							}
+							echo "<a href='moderator-action.php?id=$id&amp;action=review&amp;value=-1&amp;return_error=true&amp;redirect=$redirect_url'><li>Mark as <span style=\"font-weight: bold; color: red\">unsecure or unstable</span></li></a>";
+							echo "</ul></div>";
+							*/
+						}
+						if (hasPrivilege($_SESSION["privileges"], PRIVILEGE_STDLIB) && $item['type'] == "lib" && $item['reviewed'])
+						{
+							# insert default_include items
+							/*
+							echo "<div class=\"menu\">Library standard<ul class=\"admin-menu\">";
+							if ($item['default'])
+							{
+								echo "<a href='moderator-action.php?id=$id&amp;action=default&amp;value=0&amp;return_error=true&amp;redirect=$redirect_url'><li><span style=\"font-weight: bold; color: red\">Remove</span></li></a>";
+							}
+							else
+							{
+								echo "<a href='moderator-action.php?id=$id&amp;action=default&amp;value=1&amp;return_error=true&amp;redirect=$redirect_url'><li><span style=\"font-weight: bold; color: green\">Add</span></li></a>";
+							}
+							echo "</ul></div>";
+							*/
+						}
+					}
 			?>
 			<table>
 				<tr>
@@ -136,8 +208,15 @@
 					}
 					echo "</ul>";
 				}
+			}
 			?>
 		</div>
 		<?php require("footer.php"); ?>
 	</body>
 </html>
+<?php
+	function semver_sort($a, $b)
+	{
+		return semver_compare($b['version'], $a['version']);
+	}
+?>
