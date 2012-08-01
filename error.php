30f11670281562b2848fb1d6acc5fc0333f6032e
maul.esel <maul.esel@go4more.de>
2012-08-01 03:23:59 +0200

    error.php - encode HTML error messages

--- error.php
+++ error.php
@@ -2,7 +2,7 @@
 	<div class="message"><?php echo $error_message; ?></div>
 	<?php if (isset($error_description)) { ?>
 		<div class="description">
-			<?php echo $error_description; ?>
+			<?php echo htmlentities($error_description); ?>
 		</div>
 	<?php } ?>
 </div>
