48e59b02839ae4c37449a5461541269cb5b9e855
maul.esel <maul.esel@go4more.de>
2012-08-01 19:52:33 +0200

    fix get_API_URL()

--- get_API_URL.php
+++ get_API_URL.php
@@ -5,7 +5,7 @@
 
 		if ($secure)
 		{
-			return $_SERVER["SERVER_ADDR"] == "127.0.0.1" ? get_API_URL() : "https://{$_SERVER["SERVER_NAME"]}$site_subpath/api";
+			return $_SERVER["SERVER_ADDR"] == "127.0.0.1" ? get_API_URL() : "https://ahk4.net$site_subpath/api";
 		}
 		else
 		{
