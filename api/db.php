66e20915bf582aed779dabb5eecadbc21d93a208
maul.esel <maul.esel@go4more.de>
2012-08-01 03:22:18 +0200

    add method for retrieving enum fields from DB

--- api/db.php
+++ api/db.php
@@ -27,4 +27,18 @@
 		}
 		return $connection;
 	}
+
+	function db_get_enum_column_values($table, $column, &$values)
+	{
+		$db_connection = db_ensure_connection();
+		$db_query = "SHOW COLUMNS IN $table WHERE Field = '" . mysql_real_escape_String($column) . "'";
+		$db_result = mysql_query($db_query, $db_connection);
+		if (!$db_result)
+		{
+			return false;
+		}
+		$data = mysql_fetch_assoc($db_result);
+		$values = explode("','",substr($data["Type"],6,-2));
+		return true;
+	}
 ?>
