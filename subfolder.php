fd0120e8cb50f07985c0b44182145681c1c9ebcb
maul.esel <maul.esel@go4more.de>
2012-08-02 03:01:40 +0200

    add a few functions for handling "subfolders" in a URL

---
+++ subfolder.php
@@ -0,0 +1,17 @@
+<?php
+	require_once(dirname(__FILE__) . "/config/constants.php");
+	function get_subfolder_level()
+	{
+		$path = str_replace("?" . $_SERVER["QUERY_STRING"], "", $_SERVER["REQUEST_URI"]);
+		$sub_path = substr($path, strlen(REL_ADDRESS));
+		return substr_count($sub_path, '/');
+	}
+	function get_subfolder_prefix()
+	{
+		$prefix = "";
+		$level = get_subfolder_level();
+		for ($i = 0; $i <= $level; $i++)
+			$prefix .= "../";
+		return $prefix;
+	}
+?>
