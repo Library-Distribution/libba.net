496cfdc3470ae1e6902d163c231e0612a4375433
maul.esel <maul.esel@go4more.de>
2012-08-26 01:35:04 +0200

    add one more path constant

--- config/constants.php
+++ config/constants.php
@@ -4,6 +4,7 @@
 	define('IS_SECURE', IS_HTTPS || IS_LOCALHOST);
 
 	define('REL_ADDRESS', substr(dirname(dirname(__FILE__)), strlen(rtrim($_SERVER['DOCUMENT_ROOT'], '/'))) . '/');
+	define('RELATIVE_URL', substr($_SERVER["REQUEST_URI"], strlen(REL_ADDRESS)));
 
 	define('ROOT_URL', (IS_HTTPS ? "https" : "http") . "://"
 						. $_SERVER["SERVER_NAME"]
