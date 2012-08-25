0fba3663390c7a142591fd13e7922e2b0a363070
maul.esel <maul.esel@go4more.de>
2012-08-26 01:35:30 +0200

    slightly shorten code using the constant just added

--- subfolder.php
+++ subfolder.php
@@ -2,8 +2,7 @@
 	require_once(dirname(__FILE__) . "/config/constants.php");
 	function get_subfolder_level()
 	{
-		$path = str_replace("?" . $_SERVER["QUERY_STRING"], "", $_SERVER["REQUEST_URI"]);
-		$sub_path = substr($path, strlen(REL_ADDRESS));
+		$sub_path = str_replace("?" . $_SERVER["QUERY_STRING"], "", RELATIVE_URL);
 		return substr_count($sub_path, '/');
 	}
 	function get_subfolder_prefix()
