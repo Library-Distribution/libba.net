acef126e85133af637d0f4fd76e2bcad2e231621
maul.esel <maul.esel@go4more.de>
2013-01-06 22:12:30 +0100

    fix rewriter for absolute URLs without protocol

--- rewriter.php
+++ rewriter.php
@@ -6,7 +6,7 @@
 		$haystack = ob_get_contents();
 		$prefix = get_subfolder_prefix();
 
-		$rewritten = preg_replace("/(href|src)=(\"|')(?!(mailto|ftp|https?|\?|#|\.\/))(.+)(\"|')/isU", "$1=\"$prefix$4\"", $haystack);
+		$rewritten = preg_replace("/(href|src)=(\"|')(?!(mailto|ftp|https?|\/\/|\?|#|\.\/))(.+)(\"|')/isU", "$1=\"$prefix$4\"", $haystack);
 		ob_clean();
 		return $rewritten;
 	}
