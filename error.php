cc5b7ca5d765a7d0e009f050c649f1a58b9affbe
maul.esel <maul.esel@go4more.de>
2013-01-06 23:33:36 +0100

    better error styling
    Use jQuery-UI error styling and adjust old styling

--- error.php
+++ error.php
@@ -1,5 +1,5 @@
-<div class="error-message">
-	<div class="message"><?php echo $error_message; ?></div>
+<div class="error-message ui-state-error ui-corner-all">
+	<span class="ui-icon ui-icon-alert" style="float: left; margin-right: .3em;"></span><div class="message"><?php echo $error_message; ?></div>
 	<?php if (isset($error_description)) { ?>
 		<div class="description">
 			<?php echo htmlentities($error_description); ?>
