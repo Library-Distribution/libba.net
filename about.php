df0aaa4052c9f0557e51f3412c9474d79e3497c0
maul.esel <maul.esel@go4more.de>
2013-01-10 12:28:42 +0100

    small corrections on about page

--- about.php
+++ about.php
@@ -12,7 +12,7 @@
 		<div id="page-content">
 			<p>
 				This website is a draft for a library distribution system for <a href="http://www.autohotkey.com">AutoHotkey</a> (or <a href="http://l.autohotkey.net">AutoHotkey_L</a>, to be specific), similar to <a href="http://rubygems.org/">RubyGems</a>.
-				It's entirely written by me, maul.esel, and has been tested and developed on <a href="http://www.apachefriends.org/de/xampp.html">XAMPP</a> on Windows.
+				It's entirely written by me, maul.esel, and has been tested and developed on <a href="http://www.apachefriends.org/de/xampp.html">XAMPP</a> on Windows / Ubuntu.
 			</p>
 			<p>
 				For better visual presentation, <a href="http://andrewpaglinawan.com/#quicksand">Andrew Paglinawan's &quot;Quicksand&quot; font</a> is used. It is licensed under the <a href="font/license.txt">SIL Open Font License, Version 1.1</a>.
@@ -23,7 +23,7 @@
 				Thanks for these wonderful icons!
 			</p>
 			<p>
-				libba.net makes use of 3rd party software written in PHP an Javascript:
+				libba.net makes use of 3rd party software written in PHP, CSS and Javascript:
 				<ul>
 					<li>Thanks to <a href="http://michelf.ca">Michel Fortin</a> for <a href="http://michelf.ca/projects/php-markdown/">PHP Markdown Extra</a> (<a href="markdown/License.txt">license</a>) and <a href="http://michelf.ca/projects/php-smartypants/">PHP SmartyPants</a> (<a href="smartypants/License.txt">license</a>).</li>
 					<li>Thanks to <a href="http://ezyang.com/">Edward Z. Yang</a> for <a href="http://htmlpurifier.org">HTMLPurifier</a> (<a href="htmlpurifier/LICENSE">license</a>).</li>
