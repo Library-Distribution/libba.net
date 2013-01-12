671c491d48cf8da8dddb73568c6ceee92d3d5f10
maul.esel <maul.esel@go4more.de>
2013-01-12 01:55:36 +0100

    rewrite site navigation (and others)
    Move site navigation bar to the left instead of the top.
    Delete unnecessary header contents. completely restyle
    the new navigation and login/logout elements. Change png
    size.

--- header.php
+++ header.php
@@ -1,33 +1,55 @@
-<div id="header">
-	<div id="navigation">
-		<img alt="libba.net logo" src="images/logo.png" id="header-logo"/>
-		<div id="site-name">libba.net</div>
+<div id="navigation">
+	<a href="/" title="home page">
+		<div style="top:3%" id="nav-home">
+			<img alt="home" src="images/home.png" style="top:05%;"/>
+			<div class="nav-triangle" style="top:27.5%;">
+				<div class="nav-tooltip">Go back to the main page</div>
+			</div>
+		</div>
+	</a>
+	<a href="/items" title="libraries and applications">
+		<div style="top:23%">
+			<img alt="items" src="images/items.png" style="top:25%;"/>
+			<div class="nav-triangle" style="top:27.5%;">
+				<div class="nav-tooltip">Browse the available libraries and applications</div>
+			</div>
+		</div>
+	</a>
+	<a href="/users" title="registered users">
+		<div style="top:43%">
+			<img alt="users" src="images/users.png" style="top:45%;"/>
+			<div class="nav-triangle" style="top:27.5%;">
+				<div class="nav-tooltip">Find registered users and moderators</div>
+			</div>
+		</div>
+	</a>
+	<a href="/upload" title="package upload">
+		<div style="top:63%">
+			<img alt="upload" src="images/activity/upload.png" style="top:65%;"/>
+			<div class="nav-triangle" style="top:27.5%;">
+				<div class="nav-tooltip">Upload and share your own library or app</div>
+			</div>
+		</div>
+	</a>
+	<a href="/help" title="help index">
+		<div style="top:83%">
+			<img alt="help" src="images/help.png" style="top:85%;"/>
+			<div class="nav-triangle" style="top:27.5%;">
+				<div class="nav-tooltip">Need help with anything? This link is for you!</div>
+			</div>
+		</div>
+	</a>
+</div>
 
-		<a class="nav-link" href="index">Home</a>
-		<a class="nav-link" href="items">Code</a>
-		<a class="nav-link" href="users">Users</a>
-		<a class="nav-link" href="help">Help</a>
-		<a class="nav-link" href="upload">Upload</a>
-	</div>
-
-	<div id="header-login">
+<div id="login">
 	<?php
-		$query_start = strpos($_SERVER["REQUEST_URI"], "?");
-		$pure_url = substr($_SERVER["REQUEST_URI"], 0, $query_start);
-		$redirect = in_array($pure_url, array("/login", "/logout", "/register", "/activate"))
-						? "index"
-						: urlencode($_SERVER["REQUEST_URI"]);
-
 		if (isset($_SESSION["user"]))
 		{
-			echo "Welcome<br/><a href=\"users/{$_SESSION["user"]}/profile\">{$_SESSION["user"]}</a>!<hr/><a href=\"logout?redirect=$redirect\">Logout</a>";
+			echo "Welcome<br/><a href='users/$_SESSION[user]/profile'>$_SESSION[user]</a>!<hr/><a href='logout'>Logout</a>";
 		}
 		else
 		{
-			echo "Welcome!<hr/><a href=\"login?redirect=$redirect\">Login</a><hr/><a href=\"register?redirect=$redirect\">Register</a>";
+			echo 'Welcome!<hr/><a href="login">Login</a><hr/><a href="register">Register</a>';
 		}
 	?>
-	</div>
-
-	<a href="http://l.autohotkey.net" title="Created for AutoHotkey"><img alt="AHK" src="images/AutoHotkey.png" id="header-ahk"/></a>
 </div>
