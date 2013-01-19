292b8d7d29e2c559e9e606f08844eecf37d35d61
maul.esel <maul.esel@go4more.de>
2013-01-19 20:24:21 +0100

    add error skeleton to compare page

--- items/compare.php
+++ items/compare.php
@@ -24,98 +24,103 @@
 	* - proper error handling
 	*/
 
-	$api = new ALD(API_URL);
-	if (isset($_GET['name']) && isset($_GET['version1']) && isset($_GET['version2']))
-	{
-		try {
-			$item1 = $api->getItem($_GET['name'], $_GET['version1']);
-		} catch (HttpException $e) {
+	$page_title = "Compare ERROR";
+	$error = true;
+	for ($i = 0; $i < 1; $i++) {
+		$api = new ALD(API_URL);
+		if (isset($_GET['name']) && isset($_GET['version1']) && isset($_GET['version2']))
+		{
+			try {
+				$item1 = $api->getItem($_GET['name'], $_GET['version1']);
+			} catch (HttpException $e) {
+				# TODO: error handling
+				die('failed');
+			}
+
+			try {
+				$item2 = $api->getItem($_GET['name'], $_GET['version2']);
+			} catch (HttpException $e) {
+				# TODO: error handling
+				die('failed');
+			}
+
+			$id_old = semver_compare($item1['version'], $item2['version']) == -1 ? $item1['id'] : $item2['id'];
+			$version_old = semver_compare($item1['version'], $item2['version']) == -1 ? $item1['version'] : $item2['version'];
+			$id_new = semver_compare($item1['version'], $item2['version']) == 1 ? $item1['id'] : $item2['id'];
+			$version_new = semver_compare($item1['version'], $item2['version']) == 1 ? $item1['version'] : $item2['version'];
+			$item_name = $_GET['name'];
+		}
+		else if (isset($_GET['id1']) && isset($_GET['id2']))
+		{
+			try {
+				$item1 = $api->getItemById($_GET['id1']);
+			} catch (HttpException $e) {
+				# TODO: error handling
+				die('failed');
+			}
+
+			try {
+				$item2 = $api->getItemById($_GET['id2']);
+			} catch (HttpException $e) {
+				# TODO: error handling
+				die('failed');
+			}
+
+			if ($item1['name'] != $item2['name']) {
+				# TODO: error handling
+				die('failed');
+			}
+
+			$id_old = semver_compare($item1['version'], $item2['version']) == -1 ? $_GET['id1'] : $_GET['id2'];
+			$version_old = semver_compare($item1['version'], $item2['version']) == -1 ? $item1['version'] : $item2['version'];
+			$id_new = semver_compare($item1['version'], $item2['version']) == 1 ? $_GET['id1'] : $_GET['id2'];
+			$version_new = semver_compare($item1['version'], $item2['version']) == 1 ? $item1['version'] : $item2['version'];
+			$item_name = $item1['name'];
+		}
+		else
+		{
 			# TODO: error handling
 			die('failed');
 		}
 
-		try {
-			$item2 = $api->getItem($_GET['name'], $_GET['version2']);
-		} catch (HttpException $e) {
-			# TODO: error handling
-			die('failed');
-		}
+		$old_item = new CompareItem($api, $id_old);
+		$new_item = new CompareItem($api, $id_new);
 
-		$id_old = semver_compare($item1['version'], $item2['version']) == -1 ? $item1['id'] : $item2['id'];
-		$version_old = semver_compare($item1['version'], $item2['version']) == -1 ? $item1['version'] : $item2['version'];
-		$id_new = semver_compare($item1['version'], $item2['version']) == 1 ? $item1['id'] : $item2['id'];
-		$version_new = semver_compare($item1['version'], $item2['version']) == 1 ? $item1['version'] : $item2['version'];
-		$item_name = $_GET['name'];
-	}
-	else if (isset($_GET['id1']) && isset($_GET['id2']))
-	{
-		try {
-			$item1 = $api->getItemById($_GET['id1']);
-		} catch (HttpException $e) {
-			# TODO: error handling
-			die('failed');
-		}
+		$files_old = $old_item->files();
+		$files_new = $new_item->files();
 
-		try {
-			$item2 = $api->getItemById($_GET['id2']);
-		} catch (HttpException $e) {
-			# TODO: error handling
-			die('failed');
-		}
+		$actions = array();
+		foreach ($files_new AS $category => $files) # iterate over all files in new version
+		{
+			$actions[$category] = array();
 
-		if ($item1['name'] != $item2['name']) {
-			# TODO: error handling
-			die('failed');
-		}
-
-		$id_old = semver_compare($item1['version'], $item2['version']) == -1 ? $_GET['id1'] : $_GET['id2'];
-		$version_old = semver_compare($item1['version'], $item2['version']) == -1 ? $item1['version'] : $item2['version'];
-		$id_new = semver_compare($item1['version'], $item2['version']) == 1 ? $_GET['id1'] : $_GET['id2'];
-		$version_new = semver_compare($item1['version'], $item2['version']) == 1 ? $item1['version'] : $item2['version'];
-		$item_name = $item1['name'];
-	}
-	else
-	{
-		# TODO: error handling
-		die('failed');
-	}
-
-	$old_item = new CompareItem($api, $id_old);
-	$new_item = new CompareItem($api, $id_new);
-
-	$files_old = $old_item->files();
-	$files_new = $new_item->files();
-
-	$actions = array();
-	foreach ($files_new AS $category => $files) # iterate over all files in new version
-	{
-		$actions[$category] = array();
-
-		# check for modified or added files
-		foreach ($files AS $i => $file) {
-			if (!$old_item->hasFile($file)) {
-				$actions[$category][$file] = array('type' => ACT_ADD);
-			} else {
-				$file_old = $old_item->getFile($file);
-				$file_new = $new_item->getFile($file);
-				if ($file_old != $file_new) {
-					$ops = FineDiff::getDiffOpcodes($file_old, $file_new, FineDiff::$characterGranularity);
-					$actions[$category][$file] = array('type' => ACT_MOD, 'diff' => html_entity_decode(FineDiff::renderDiffToHTMLFromOpcodes($file_old, $ops)));
+			# check for modified or added files
+			foreach ($files AS $i => $file) {
+				if (!$old_item->hasFile($file)) {
+					$actions[$category][$file] = array('type' => ACT_ADD);
+				} else {
+					$file_old = $old_item->getFile($file);
+					$file_new = $new_item->getFile($file);
+					if ($file_old != $file_new) {
+						$ops = FineDiff::getDiffOpcodes($file_old, $file_new, FineDiff::$characterGranularity);
+						$actions[$category][$file] = array('type' => ACT_MOD, 'diff' => html_entity_decode(FineDiff::renderDiffToHTMLFromOpcodes($file_old, $ops)));
+					}
 				}
 			}
-		}
 
-		# check for deleted files
-		foreach ($files_old[$category] AS $i => $file) {
-			if (!$new_item->hasFile($file)) {
-				$actions[$category][$file] = array('type' => ACT_DEL);
+			# check for deleted files
+			foreach ($files_old[$category] AS $i => $file) {
+				if (!$new_item->hasFile($file)) {
+					$actions[$category][$file] = array('type' => ACT_DEL);
+				}
 			}
+
+			asort($actions[$category]);
 		}
 
-		asort($actions[$category]);
+		$page_title = "Comparing $item_name v$version_old with v$version_new";
+		$error = false;
 	}
-
-	$page_title = "Comparing $item_name v$version_old with v$version_new";
 ?>
 <!DOCTYPE html>
 <html>
@@ -130,30 +135,37 @@
 	<body class="pretty-ui">
 		<h1 id="page-title"><?php echo $page_title; ?></h1>
 		<div id="page-content">
-			<ul id="diff-steps">
-				<?php
-					foreach($actions AS $category => $steps) {
-						if (count($steps) > 0) {
-							echo '<h3>', $category, '</h3>';
-						}
-						foreach ($steps AS $file => $action) {
-							switch ($action['type']) {
-								case ACT_ADD: $prefix = 'Added:';
-											$details = "File <code>$file</code> has been added to the package.";
-											break;
-								case ACT_DEL: $prefix = 'Deleted:';
-											$details = "File <code>$file</code> has been deleted from the package.";
-											break;
-								default:
-								case ACT_MOD: $prefix = 'Modified:';
-											$details = '<div class="compare-diff">' . $action['diff'] . '</div>';
-											break;
+			<?php
+				if ($error)
+				{
+					require('../error.php');
+				} else {
+			?>
+				<ul id="diff-steps">
+					<?php
+						foreach($actions AS $category => $steps) {
+							if (count($steps) > 0) {
+								echo '<h3>', $category, '</h3>';
 							}
-							echo '<li><span class="action-summary"><span class="action-type">' . $prefix . '</span> ' . $file . '</span><div class="action-details">' . $details . '</div></li>';
+							foreach ($steps AS $file => $action) {
+								switch ($action['type']) {
+									case ACT_ADD: $prefix = 'Added:';
+												$details = "File <code>$file</code> has been added to the package.";
+												break;
+									case ACT_DEL: $prefix = 'Deleted:';
+												$details = "File <code>$file</code> has been deleted from the package.";
+												break;
+									default:
+									case ACT_MOD: $prefix = 'Modified:';
+												$details = '<div class="compare-diff">' . $action['diff'] . '</div>';
+												break;
+								}
+								echo '<li><span class="action-summary"><span class="action-type">' . $prefix . '</span> ' . $file . '</span><div class="action-details">' . $details . '</div></li>';
+							}
 						}
-					}
-				?>
-			</ul>
+					?>
+				</ul>
+			<?php } ?>
 		</div>
 	</body>
 </html>
