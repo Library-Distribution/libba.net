5b7e830dce643c3c3690e314f3f1621b95ffbea7
maul.esel <maul.esel@go4more.de>
2013-01-28 20:43:23 +0100

    remove old debug code from login.js

--- javascript/login.js
+++ javascript/login.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 	if (!Modernizr.input.list) {
 		var users = $.makeArray($("datalist#registered-users option").map(function(i, item) { return $(item).attr("value"); }));
-		console.log(users);
 		$("input#user-name").autocomplete({ source: users });
 	}
 });
