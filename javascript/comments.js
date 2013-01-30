bce59c91a94cc924d6709046838a5b6d7ad69332
maul.esel <maul.esel@go4more.de>
2013-01-30 17:08:03 +0100

    comment preview: add display options
    Enable the user to show the preview permanently or never.

--- javascript/comments.js
+++ javascript/comments.js
@@ -8,6 +8,17 @@ function _load_comment_preview_() {
 }
 
 function EnableCommentPreviews() {
-	$(".preview-source").after($("<div class='preview-block'><h3>Comment preview:</h3><div class='markdown'><span class='preview-empty'>Nothing to preview yet. Type in the box above and see the preview here...</span></div></div>"));
-	$(".preview-source").change(_load_comment_preview_).keyup(_load_comment_preview_);
+	// add and enable the preview block
+	$(".preview-source").after("<div class='preview-block'><h3>Comment preview:</h3><div class='markdown'><span class='preview-empty'>Nothing to preview yet. Type in the box above and see the preview here...</span></div></div>")
+	.change(_load_comment_preview_).keyup(_load_comment_preview_);
+
+	// add preview options
+	$(".preview-source").before("preview: <label style='display: inline-block'><input type='radio' name='previewMode'/>permanently</label>"
+							+ "<label style='display:inline-block'><input type='radio' name='previewMode' checked='checked'/>while editing</label>"
+							+ "<label style='display:inline-block'><input type='radio' name='previewMode'/>never</label>");
+
+	// enable preview options
+	$("form").has(".preview-source").change(function() { // attach event handler to forms with preview sources
+		$(".preview-source + .preview-block", this).css("display", this.previewMode[0].checked ? "block" : (this.previewMode[1].checked ? "" : "none"));
+	});
 }
