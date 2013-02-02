072cdf34d7eea4aa3fc618f08775763ff290bcc3
maul.esel <maul.esel@go4more.de>
2013-02-02 23:04:16 +0100

    change sortArray to support PHP 5.2
    Add a workaround for not using anonymous functions.

--- util/sortArray.php
+++ util/sortArray.php
@@ -1,27 +1,39 @@
 <?php
 # This function was taken from http://www.the-art-of-web.com/php/sortarray/ (2012/06/06) and has been created by Thomas Heuer (Germany). Thanks a lot for this!
-#It has been modified by me (maul.esel) to use objects instead of arrays and to allow both ascending and descending sorting.
+#It has been modified by me (maul.esel) to use objects instead of arrays, to allow both ascending and descending sorting and to work without anonymous functions.
 function sortArray($data, $field)
 {
 	if (!is_array($field))
 	{
 		$field = array($field => false);
 	}
-	usort($data, function($a, $b) use($field)
-	{
-		$retval = 0;
-		foreach($field as $fieldname => $direction)
-		{
-			if($retval == 0)
-			{
-				if (!$direction)
-					$retval = strnatcasecmp($a[$fieldname], $b[$fieldname]);
-				else
-					$retval = strnatcasecmp($b[$fieldname], $a[$fieldname]);
-			}
-		}
-		return $retval;
-	});
+	sortArray_storage($field);
+	usort($data, "sortArray_sort");
 	return $data;
+}
+
+function sortArray_sort($a, $b)
+{
+	$retval = 0;
+	$fields = sortArray_storage();
+	foreach($fields as $fieldname => $direction)
+	{
+		if($retval == 0)
+		{
+			if (!$direction)
+				$retval = strnatcasecmp($a[$fieldname], $b[$fieldname]);
+			else
+				$retval = strnatcasecmp($b[$fieldname], $a[$fieldname]);
+		}
+	}
+	return $retval;
+}
+
+function sortArray_storage($fields = NULL)
+{
+	static $storage = NULL;
+	if ($fields != NULL)
+		$storage = $fields;
+	return $storage;
 }
 ?>
