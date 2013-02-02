8668f3059d776a2262890299fa04582434a0c64d
maul.esel <maul.esel@go4more.de>
2013-02-02 01:37:38 +0100

    error and warning styling
    Improve error styling, add a warning template.
    Change both to be accessible as functions, de-
    fined in one central file in `partials/`.

--- error.php
+++
@@ -1,8 +0,0 @@
-<div class="error-message ui-state-error ui-corner-all">
-	<span class="ui-icon ui-icon-alert" style="float: left; margin-right: .3em;"></span><div class="message"><?php echo $error_message; ?></div>
-	<?php if (isset($error_description)) { ?>
-		<div class="description">
-			<?php echo htmlentities($error_description); ?>
-		</div>
-	<?php } ?>
-</div>
