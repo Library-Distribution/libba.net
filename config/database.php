b62017001a9160f8836f57b9d6be3b92fd270fb1
maul.esel <maul.esel@go4more.de>
2013-02-04 21:27:26 +0100

    add database configuration file

---
+++ config/database.php
@@ -0,0 +1,17 @@
+<?php
+	# the name of the MySQL database to use
+	define('DB_NAME', 'ald');
+
+	# the name of the database server
+	define('DB_SERVER', 'localhost');
+
+	# the names of the database tables
+	define('DB_TABLE_CANDIDATES', 'site_stdlib_cand'); # 
+	define('DB_TABLE_CANDIDATE_COMMENTS', 'site_stdlib_cand_comments'); # 
+	define('DB_TABLE_REVIEW_COMMENTS', 'site_review_comments'); # 
+	define('DB_TABLE_USER_PROFILE', 'site_user_profile'); # 
+
+	# the credentials for accessing the database
+	define('DB_USERNAME', 'root'); # the user name
+	define('DB_PASSWORD', ''); # the password
+?>
--- util/db2.php
+++ util/db2.php
@@ -1,6 +1,8 @@
 <?php
-	$db_table_candidates = "site_stdlib_cand";
-	$db_table_candidate_comments = "site_stdlib_cand_comments";
-	$db_table_review_comments = "site_review_comments";
-	$db_table_user_profile = "site_user_profile";
+	require_once(dirname(__FILE__) . '/../config/database.php');
+
+	$db_table_candidates = DB_TABLE_CANDIDATES;
+	$db_table_candidate_comments = DB_TABLE_CANDIDATE_COMMENTS;
+	$db_table_review_comments = DB_TABLE_REVIEW_COMMENTS;
+	$db_table_user_profile = DB_TABLE_USER_PROFILE;
 ?>
