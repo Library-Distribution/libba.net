d2e09065986f5081ae758df28d1ef54a785d9370
maul.esel <maul.esel@go4more.de>
2013-05-22 17:31:16 +0200

    update html.head partial for use with templates

--- partials/html.head.php
+++ partials/html.head.php
@@ -1,3 +1,10 @@
-<meta charset="UTF-8"/>
-<title><?php echo $page_title; ?> | libba.net</title>
-<link rel="stylesheet" type="text/css" href="style/default.css"/>
+<head>
+	<meta charset='UTF-8'/>
+	<title><?php echo $template['page_title']; ?> | libba.net</title>
+<?php foreach ($template['styles'] AS $style) { ?>
+	<link rel='stylesheet' type='text/css' href='<?php echo $style; ?>'/>
+<?php }
+      foreach ($template['scripts'] AS $script) { ?>
+	<script type='text/javascript' src='<?php echo $script; ?>'></script>
+<?php } ?>
+</head>
