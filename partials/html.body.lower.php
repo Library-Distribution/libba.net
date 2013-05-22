fede2a139d2277498f9119ef939d79f8a2440ed2
maul.esel <maul.esel@go4more.de>
2013-05-22 17:31:43 +0200

    add more template partials

---
+++ partials/html.body.lower.php
@@ -0,0 +1,7 @@
+	<!-- ... -->
+	</div>
+	<?php
+		require(dirname(__FILE__) . '/header.php');
+		require(dirname(__FILE__) . '/footer.php');
+	?>
+</body>
---
+++ partials/html.body.upper.php
@@ -0,0 +1,4 @@
+<body>
+	<h1 id='page-title'><?php echo $template['page_title']; ?></h1>
+	<div id='page-content'>
+	<!-- ... -->
